0d082c94ac3db9bcc4c7c14f537a1005b1f16906
jackylx2008 <jackylx2008@hotmail.com>
2024-07-25 15:52:41 +0800

    2024-07-25_operator=

--- 2024-07-20_CPP_13/DMA/dma.cpp
+++ 2024-07-20_CPP_13/DMA/dma.cpp
@@ -9,97 +9,91 @@ static int t = 0;
 const int __TEST__ = false;
 
 /* BaseDma */
-BaseDma::BaseDma(const char* s, int n) {
-    if (__TEST__) {
-        std::cout << "args BaseDma" << std::endl;
-    }
-    str = new char(strlen(s + 1));
-    strcpy(str, s);
-    num = n;
+BaseDma::BaseDma(const char *s, int n) {
+  if (__TEST__) {
+    std::cout << "args BaseDma" << std::endl;
+  }
+  str = new char(strlen(s + 1));
+  strcpy(str, s);
+  num = n;
 }
-BaseDma::BaseDma(const BaseDma& ba) {
-    if (__TEST__) {
-        std::cout << "constrcut ba" << std::endl;
-    }
-    delete[] str;
-    str = new char(strlen(ba.str + 1));
-    strcpy(str, ba.str);
-    num = ba.num;
+BaseDma::BaseDma(const BaseDma &ba) {
+  if (__TEST__) {
+    std::cout << "constrcut ba" << std::endl;
+  }
+  delete[] str;
+  str = new char(strlen(ba.str + 1));
+  strcpy(str, ba.str);
+  num = ba.num;
 }
-BaseDma& BaseDma::operator=(const BaseDma& ba) {
-    if (__TEST__) {
-        std::cout << "operator=ba" << std::endl;
-    }
-    if (this == &ba) {
-        return *this;
-    }
-    delete[] str;
-    str = new char(strlen(ba.str + 1));
-    strcpy(str, ba.str);
-    num = ba.num;
+BaseDma &BaseDma::operator=(const BaseDma &ba) {
+  if (__TEST__) {
+    std::cout << "operator=ba" << std::endl;
+  }
+  if (this == &ba) {
     return *this;
+  }
+  delete[] str;
+  str = new char(strlen(ba.str + 1));
+  strcpy(str, ba.str);
+  num = ba.num;
+  return *this;
 }
-BaseDma::~BaseDma() {
-    delete[] str;
-}
-std::ostream& operator<<(std::ostream& os, const BaseDma& ba) {
-    os << "Str : " << ba.str << std::endl;
-    os << "Num : " << ba.num << std::endl;
-    return os;
+BaseDma::~BaseDma() { delete[] str; }
+std::ostream &operator<<(std::ostream &os, const BaseDma &ba) {
+  os << "Str : " << ba.str << std::endl;
+  os << "Num : " << ba.num << std::endl;
+  return os;
 }
 /* Derived_Ba */
-Derived_Ba::Derived_Ba(const char* s, int n, int m) : BaseDma(s, n), m_m(m) {
+Derived_Ba::Derived_Ba(const char *s, int n, int m) : BaseDma(s, n), m_m(m) {}
+Derived_Ba::Derived_Ba(const BaseDma &ba, int m) : BaseDma(ba), m_m(m) {}
+Derived_Ba::Derived_Ba(const Derived_Ba &da) {
+  BaseDma::operator=((const BaseDma &)da);
+  m_m = da.m_m;
 }
-Derived_Ba::Derived_Ba(const BaseDma& ba, int m) : BaseDma(ba), m_m(m) {
-}
-Derived_Ba::Derived_Ba(const Derived_Ba& da) {
-    BaseDma::operator=((const BaseDma&)da);
-    m_m = da.m_m;
-}
-std::ostream& operator<<(std::ostream& os, const Derived_Ba& da) {
-    os << (const BaseDma&)da;
-    os << da.m_m << std::endl;
-    return os;
+std::ostream &operator<<(std::ostream &os, const Derived_Ba &da) {
+  os << (const BaseDma &)da;
+  os << da.m_m << std::endl;
+  return os;
 }
 
 /* Derived_New */
-Derived_New::Derived_New(const char* s, int n, const char* ns) : BaseDma(s, n) {
-    std::cout << "Derived_New " << ++t << std::endl;
-    delete[] new_str;
-    new_str = new char(strlen(ns + 1));
-    strcpy(new_str, ns);
+Derived_New::Derived_New(const char *s, int n, const char *ns) : BaseDma(s, n) {
+  std::cout << "Derived_New " << ++t << std::endl;
+  delete[] new_str;
+  new_str = new char(strlen(ns + 1));
+  strcpy(new_str, ns);
 }
-Derived_New::Derived_New(const BaseDma& ba, const char* ns) : BaseDma(ba) {
-    delete[] new_str;
-    new_str = new char(strlen(ns + 1));
-    strcpy(new_str, ns);
+Derived_New::Derived_New(const BaseDma &ba, const char *ns) : BaseDma(ba) {
+  delete[] new_str;
+  new_str = new char(strlen(ns + 1));
+  strcpy(new_str, ns);
 }
-Derived_New::Derived_New(const Derived_New& dn) : BaseDma((const BaseDma&)dn) {
-    delete[] new_str;
-    new_str = new char(strlen(dn.new_str + 1));
-    strcpy(new_str, dn.new_str);
+Derived_New::Derived_New(const Derived_New &dn) : BaseDma((const BaseDma &)dn) {
+  delete[] new_str;
+  new_str = new char(strlen(dn.new_str + 1));
+  strcpy(new_str, dn.new_str);
 }
-Derived_New& Derived_New::operator=(const Derived_New& dn) {
-    if (this == &dn) {
-        return *this;
-    }
-    // BaseDma((const BaseDma&)dn);
-    delete[] str;
-    str = new char(strlen(dn.str + 1));
-    strcpy(str, dn.str);
-
-    num = dn.num;
-
-    delete[] new_str;
-    new_str = new char(strlen(dn.new_str + 1));
-    strcpy(new_str, dn.new_str);
+Derived_New &Derived_New::operator=(const Derived_New &dn) {
+  if (this == &dn) {
     return *this;
+  }
+  BaseDma::operator=((const BaseDma &)dn);
+  // delete[] str;
+  // str = new char(strlen(dn.str + 1));
+  // strcpy(str, dn.str);
+  //
+  // num = dn.num;
+
+  delete[] new_str;
+  new_str = new char(strlen(dn.new_str + 1));
+  strcpy(new_str, dn.new_str);
+  return *this;
 }
-Derived_New::~Derived_New() {
-    delete[] new_str;
-}
-std::ostream& operator<<(std::ostream& os, const Derived_New& dn) {
-    os << (const BaseDma&)dn;
-    os << dn.new_str << std::endl;
-    return os;
+Derived_New::~Derived_New() { delete[] new_str; }
+std::ostream &operator<<(std::ostream &os, const Derived_New &dn) {
+  os << (const BaseDma &)dn;
+  os << dn.new_str << std::endl;
+  return os;
 }
--- 2024-07-20_CPP_13/DMA/dma.h
+++ 2024-07-20_CPP_13/DMA/dma.h
@@ -4,42 +4,41 @@
 #include <ostream>
 
 class BaseDma {
-  protected:
-    char* str = nullptr;
-    int num;
+private:
+  char *str = nullptr;
+  int num;
 
-  public:
-    explicit BaseDma(const char* s = "", int n = 0);
-    BaseDma(const BaseDma& ba);
-    BaseDma& operator=(const BaseDma& ba);
-    virtual ~BaseDma();
-    friend std::ostream& operator<<(std::ostream& os, const BaseDma& ba);
+public:
+  explicit BaseDma(const char *s = "", int n = 0);
+  BaseDma(const BaseDma &ba);
+  BaseDma &operator=(const BaseDma &ba);
+  virtual ~BaseDma();
+  friend std::ostream &operator<<(std::ostream &os, const BaseDma &ba);
 };
 
 class Derived_Ba : public BaseDma {
-  private:
-    int m_m;
+private:
+  int m_m;
 
-  public:
-    explicit Derived_Ba(const char* s = "", int n = 0, int m = 0);
-    Derived_Ba(const BaseDma& ba, int m);
-    Derived_Ba(const Derived_Ba& da);
-    virtual ~Derived_Ba() {
-    }
-    friend std::ostream& operator<<(std::ostream& os, const Derived_Ba& da);
+public:
+  explicit Derived_Ba(const char *s = "", int n = 0, int m = 0);
+  Derived_Ba(const BaseDma &ba, int m);
+  Derived_Ba(const Derived_Ba &da);
+  virtual ~Derived_Ba() {}
+  friend std::ostream &operator<<(std::ostream &os, const Derived_Ba &da);
 };
 class Derived_New : public BaseDma {
-  private:
-    char* new_str = nullptr;
+private:
+  char *new_str = nullptr;
 
-  public:
-    explicit Derived_New(const char* s = "null", int n = 0,
-                         const char* ns = "null");
-    Derived_New(const BaseDma& ba, const char* ns);
-    Derived_New(const Derived_New& dn);
-    Derived_New& operator=(const Derived_New& dn);
-    virtual ~Derived_New();
-    friend std::ostream& operator<<(std::ostream& os, const Derived_New& dn);
+public:
+  explicit Derived_New(const char *s = "null", int n = 0,
+                       const char *ns = "null");
+  Derived_New(const BaseDma &ba, const char *ns);
+  Derived_New(const Derived_New &dn);
+  Derived_New &operator=(const Derived_New &dn);
+  virtual ~Derived_New();
+  friend std::ostream &operator<<(std::ostream &os, const Derived_New &dn);
 };
 
-#endif  // !__DMA_H__
+#endif // !__DMA_H__
