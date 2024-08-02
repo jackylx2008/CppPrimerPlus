8aa8a10babc6de4c84440e5c192ead1af8335e0d
jackylx2008 <jackylx2008@hotmail.com>
2024-08-02 15:43:35 +0800

    2024-08-02 private inheritance

---
+++ 2024-07-30_CPP_14/Student-private_derived_class/student.cpp
@@ -0,0 +1,43 @@
+#include <istream>
+#include <ostream>
+#include <valarray>
+
+#include "./student.h"
+
+double Student::Average() const {
+    if (std::valarray<double>::size() > 0) {
+        return std::valarray<double>::sum() / std::valarray<double>::size();
+    } else {
+        return 0.0;
+    }
+}
+double& Student::operator[](int n) {
+    return std::valarray<double>::operator[](n);
+}
+const double Student::operator[](int n) const {
+    return std::valarray<double>::operator[](n);
+}
+std::istream& operator>>(std::istream& is, Student& s) {
+    is >> (std::string&)s;
+    return is;
+}
+std::istream& getline(std::istream& is, Student& s) {
+    getline(is, (std::string&)s);
+    return is;
+}
+std::ostream& Student::show_Scores(std::ostream& os) const {
+    os << "Name: " << (const std::string&)*this;
+    os << " Scores:";
+    if (std::valarray<double>::size() > 0) {
+        for (int i = 0; i < std::valarray<double>::size(); i++) {
+            os << std::valarray<double>::operator[](i) << " ";
+        }
+    } else {
+        os << " 0.0";
+    }
+    return os;
+}
+std::ostream& operator<<(std::ostream& os, const Student& s) {
+    s.show_Scores(os);
+    return os;
+}
---
+++ 2024-07-30_CPP_14/Student-private_derived_class/student.h
@@ -0,0 +1,37 @@
+#ifndef __STUDENT_VALARRAY_H__
+#define __STUDENT_VALARRAY_H__
+
+#include <string>
+#include <valarray>
+
+class Student : private std::string, private std::valarray<double> {
+  private:
+  public:
+    Student() : std::string("Null"), std::valarray<double>() {}
+    explicit Student(const std::string name, int n)
+        : std::string(name), std::valarray<double>(n) {}
+    explicit Student(int n) : std::string("Null"), std::valarray<double>(n) {}
+    explicit Student(const std::string& name,
+                     const std::valarray<double>& scores)
+        : std::string(name), std::valarray<double>(scores) {}
+    Student(std::string name, const double* pd, int n)
+        : std::string(name), std::valarray<double>(pd, n) {}
+    Student(const Student& s)
+        : std::string((const std::string&)s),
+          std::valarray<double>((const std::valarray<double>&)s) {}
+
+    ~Student() {}
+    double Average() const;
+    const std::string& get_Name() const {
+        return (const std::string&)*this;
+    }
+    std::ostream& show_Scores(std::ostream& os) const;
+    // Student& operator=(const Student& s);
+    double& operator[](int n);
+    const double operator[](int n) const;
+    friend std::istream& operator>>(std::istream& is, Student& s);
+    friend std::istream& getline(std::istream& is, Student& s);
+    friend std::ostream& operator<<(std::ostream& os, const Student& s);
+};
+
+#endif  // !__STUDENT_VALARRAY_H__
